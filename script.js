7381adbb06cc17b4b6a8add63269145237b6f2f9
leeeseonmin <163627487+leeeseonmin@users.noreply.github.com>
2024-05-25 07:20:22 +0000

    20240525

---
+++ script.js
@@ -0,0 +1,33 @@
+//console.log('HELLO');
+//Commentary
+//alert('hello');
+var a = 10;
+var b;
+b = 100;
+console.log (a + b);
+console.log (a / b);
+console.log (7 % 2);
+console.log (5 % 3);
+console.log (3 % 2);
+console.log (7 % 4);
+console.log (6 % 2);
+console.log (7 > 3);
+console.log (7 < 3);
+console.log (7 >= 7);
+console.log (3 <= 3);
+console.log (7 == 7);
+console.log (7 != 7);
+console.log (7 == '7');
+console.log (7 === '7');
+//=== 3개를 써야 형태까지 같은지 확인됩니다.
+인사 = 'hello';
+console.log (인사); //언어팩 안깔려있는 경우가 많으니 한글 사용할 생각마세요.
+console.log (7+parseInt('2', 10));
+console.log ((3).toString());
+var name = 'Gooma';
+console.log (`안녕하세요, ${name}`);
+//문자열 쓸 때 ``  백쿼터씁니다. 값은 중괄호{} 씁니다.
+console.log ('안녕하세요' + name);
+var p = 'cat dog hamster alligator';
+console.log(p.indexOf('dog'));
+console.log(p.indexOf('cat'));
